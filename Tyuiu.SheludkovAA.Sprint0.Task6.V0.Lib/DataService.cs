1dfabbb033eadf741ece45e1cc6c325724bcd5de
Tyuiu.SheludkovAA@outlook.com <Tyuiu.SheludkovAA@outlook.com>
2023-09-11 22:24:11 +0500

    Task6

---
+++ Tyuiu.SheludkovAA.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SheludkovAA.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static object AdditionArray(int[] numbers)
+        {
+            var total = 0;
+            for (var i = 0; i < numbers.Length; i++) // изначально 0 и пока i не будет больше длины массива наращиваем
+            {
+                total = total + numbers[i];
+            }
+            return total;
+        }
+        public static object SubtractionArray(int[] numbers)
+        {
+            var total = 0;
+            int index = 0;
+            while (index < numbers.Length) //пока индекс не будет равен длине массива
+            {
+                total = total - numbers[index]; 
+                index++; //привавляем к индексу единичку 
+            }
+            return total;
+        }
+        public static object MultiplicationArray(int[] numbers)
+        {
+            var total = 1;
+            int index = 0;
+            do
+            {
+                total = total * numbers[index];
+                index++;
+            }
+            while (index < numbers.Length);//выполнится хотя бы раз
+
+            return total;
+        }
+    }
+}
---
+++ Tyuiu.SheludkovAA.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SheludkovAA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SheludkovAA.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0341af8-5e78-4656-93de-fb678fd2534f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
